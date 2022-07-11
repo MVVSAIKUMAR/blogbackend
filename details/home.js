a9618c043b940cd7d01086e4b6924930e33c9458
MVVSAIKUMAR <mvvsaikumar9@gmail.com>
2022-07-11 20:39:44 +0530

    first commit

--- details/home.js
+++ details/home.js
@@ -9,30 +9,13 @@ const get = (req,res)=>{
             para1:"Kerala, southwestern coastal state of India. It is a small state, constituting only about 1 percent of the total area of the country. Kerala stretches for about 360 miles (580 km) along the Malabar Coast, varying in width from roughly 20 to 75 miles (30 to 120 km). It is bordered by the states of Karnataka (formerly Mysore) to the north and Tamil Nadu to the east and by the Arabian Sea to the south and west.",
             para2:"With all elements of nature in it, Kerala leads the country in various aspects like literacy rate, sex ratio, least population growth, and travel & tourism. Kerala is known as Keralam in the Malayalam Language. Kerala has so many breathtaking destinations that anyone who wants to explore new places should visit Kerala once in his/her lifetime and add it to their bucket list."
         },{
-            id:4,
-            name:"Madhya Pradesh: Heart of India",
-            info:"visit Madhya Pradesh",
-            img:"https://tourism.mp.gov.in/images/Gallary/FooterGallery/01082018060705MAHESHWAR%20-%20FORT.jpg",
-            display_text:"Madhya Pradesh is known as the “Heart of India” because of a lot many things that it has to offer. It has been home to cultural and spiritual heritage of almost all the religions.",
-            para1:"The natural beauty of the state is equally varied. Consisting largely of a plateau, the State has everything, from mighty mountain ranges to meandering rivers to miles of verdant forests. In fact a large part of Madhya Pradesh is under the forest cover, offering a unique and exciting panorama of wildlife.",
-            para2:" Let the hands of time which have rested lightly on the chhatris, palaces and forts of Orchha and Gwalior, whisper the tales of medieval India. The temples of Khajuraho are India’s unique gift to the world, representing love and joys of life in a sublime expression."
-        },
-        {
-            id:7,
-            name:"Areobics",
-            info:"you must do aerobics",
-            img:"https://previews.123rf.com/images/lentolo/lentolo1212/lentolo121200041/16756579-aerobics-class-in-einem-gym-italien.jpg",
-            display_text:"Aerobics is a form of physical exercise that combines rhythmic aerobic exercise with stretching and strength training routines with the goal of improving all elements of fitness (flexibility, muscular strength, and cardio-vascular fitness).",
-            para1:"Aerobic or with oxygen exercises provide cardiovascular conditioning. The American Heart Association recommends a minimum of 30 minutes of cardiovascular exercise 5 to 7 days per week. Don't forget warm-up, cool-down and stretching exercises in your aerobic exercise session.",
-            para2:"The intensity is determined by how hard you are working. The intensity of the exercise is determined by what your goals are, what limitations you have, and your current fitness level."
-        },{
-            id:9,
-            name:"Sprint",
-            info:"sprint, also called dash, in athletics (track and field), a footrace over a short distance.",
-            img:"https://s2.reutersmedia.net/resources/r/?m=02&d=20210806&t=2&i=1571227967&w=&fh=545&fw=810&ll=&pl=&sq=&r=2021-08-06T191713Z_41363_MRPRC2RZO9T5T55_RTRMADP_0_OLYMPICS-2020-EDITORS-CHOICE",
-            display_text:"It is used in many sports that incorporate running, typically as a way of quickly reaching a target or goal, or avoiding or catching an opponent.",
-            para1:"The 400m oval running track is split into eight lanes, where each lane is 4ft wide. Up to eight athletes compete in a single race. Competitions are conducted in a heats format, where athletes in groups of eight take part in a every race, with winners moving on to the next round, until the final winner is decided.",
-            para2:"Sprint races can be of various distances from 50 - 400m. The three formats used for the Olympics are: 100m, 200m and 400m. The 100m and 400m races are also conducted in a relay format where a team of four each run a leg and pass a baton from one runner to the next."
+            id:6,
+            name:"Yoga",
+            info:"you must do yoga",
+            img:"https://images.sadhguru.org/d/46272/1633197086-1633197085450.jpg",
+            display_text:"Yoga is a mind and body practice. Various styles of yoga combine physical postures, breathing techniques, and meditation or relaxation. Yoga is an ancient practice that may have originated in India. It involves movement, meditation, and breathing techniques to promote mental and physical well-being.",
+            para1:"Yoga has been around for millennia. According to some records, it was developed in Northern India over 5,000 years ago, and according to data published in 2004 in the journal Alternative Therapies in Health and Medicine, an estimated 15 million American adults have used yoga at least once in their lifetime.",
+            para2:"and more than 7.4 million participated in the year prior to when the research was conducted. Individuals interviewed for this research reported that they used yoga for wellness (stressreduction, quality of life), health conditions, and specific ailments like back or neck pain, and 90% felt yoga was very or somewhat helpful."
         },{
             id:11,
             name:"VadaPav",
@@ -41,22 +24,6 @@ const get = (req,res)=>{
             display_text:"If you are not a Mumbaikar, this is something which should not be skipped. Whether it”s for breakfast, lunch or teatime, Batata Vada can go with anything.Vada pav, alternatively spelt wada pao, is a vegetarian fast food dish native to the state of Maharashtra. The dish consists of a deep fried potato dumpling placed inside a bread bun sliced almost in half through the middle. It is generally accompanied with one or more chutneys and a green chili pepper.",
             para1:"Vada pav, alternatively spelt wada pao, is a vegetarian fast food dish native to the state of Maharashtra. The dish consists of a deep fried potato dumpling placed inside a bread bun sliced almost in half through the middle. It is generally accompanied with one or more chutneys and a green chili pepper.",
             para2:"The carbohydrate-rich snack catered to the cotton mill workers of what was then known as Girangaon. This potato dumpling (batata vada) placed inside a pav was quick to make, cheap, and much convenient over the batata bhaji and chapati combination, which couldn't be eaten in overcrowded local trains."
-        },{
-            id:14,
-            name:"Tandoori Chicken",
-            info:"The chicken is marinated in yogurt and seasoned with the spice mixture tandoori masala.",
-            img:"https://www.licious.in/blog/wp-content/uploads/2021/01/shutterstock_400329463-min-1.jpg",
-            display_text:"Tandoori chicken is a popular North Indian dish consisting of roasted chicken prepared with yogurt and spices. The name comes from the type of cylindrical clay oven, a tandoor, in which the dish is traditionally prepared.Sounds unbelievable, but historians are said to have found the first evidence of a meat preparation which looked like Tandoori Chicken in the ruins of Harappa.",
-            para1:"Sounds unbelievable, but historians are said to have found the first evidence of a meat preparation which looked like Tandoori Chicken in the ruins of Harappa.",
-            para2:"Ancient Sanskrit treatise Sushruta Samhita mentions meat marinated with mustard powder and fragrant spices being cooked in clay ovens."
-        },{
-            id:19,
-            name:"Artificial intelligence",
-            info:"Artificial intelligence is intelligence demonstrated by machines.",
-            img:"https://www.sciencenewsforstudents.org/wp-content/uploads/2021/08/LL_AI_feat-1030x580.jpg",
-            display_text:"Artificial intelligence (AI) is intelligence demonstrated by machines, as opposed to natural intelligence displayed by animals including humans. Leading AI textbooks define the field as the study of intelligent agents: any system that perceives its environment and takes actions that maximize its chance of achieving its goals.Some popular accounts use the term artificial intelligence to describe machines that mimic cognitive functions that humans associate with the human mind, such as learning and problem solving, however, this definition is rejected by major AI researchers.",
-            para1:"The major limitation in defining AI as simply building machines that are intelligent is that it doesn't actually explain what artificial intelligence is? What makes a machine intelligent? AI is an interdisciplinary science with multiple approaches, but advancements in machine learning and deep learning are creating a paradigm shift in virtually every sector of the tech industry. ",
-            para2:"However, decades before this definition, the birth of the artificial intelligence conversation was denoted by Alan Turing's seminal work, Computing Machinery and Intelligence (PDF, 89.8 KB)(link resides outside of IBM), which was published in 1950. In this paper, Turing, often referred to as the father of computer science, asks the following question, Can machines think?  From there, he offers a test, now famously known as the Turing Test, where a human interrogator would try to distinguish between a computer and human text response. While this test has undergone much scrutiny since its publish, it remains an important part of the history of AI as well as an ongoing concept within philosophy as it utilizes ideas around linguistics."
         }
     ])
 }
